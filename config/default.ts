6cf752385e717b4d8f3c356a2871bb070435b177
rojasadrian012 <rojasadrian012@gmail.com>
2025-07-14 12:39:38 -0300

    Update accessToken expiration time to 120 seconds

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
   origin: 'http://localhost:3000',
-  accessTokenExpiresIn: 15,
+  accessTokenExpiresIn: 120,
   refreshTokenExpiresIn: 60,
   redisCacheExpiresIn: 60,
   emailFrom: 'contact@codevoweb.com',
