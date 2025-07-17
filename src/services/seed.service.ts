cd81850dde4b6388055583da00750a45d9555fe0
rojasadrian012 <rojasadrian012@gmail.com>
2025-07-17 12:45:41 -0300

    Add userQuestionResponse repository to seed service for data management

--- src/services/seed.service.ts
+++ src/services/seed.service.ts
@@ -2,11 +2,15 @@ import { DeepPartial } from "typeorm";
 import { Question } from "../entities/question.entity";
 import { AppDataSource } from "../utils/data-source";
 import { Option } from "../entities/options.entity";
+import { UserQuestionResponse } from "../entities/userQuestionResponse.entity";
 
 const questionRepository = AppDataSource.getRepository(Question);
 const optionRepository = AppDataSource.getRepository(Option);
+const userQuestionRepository = AppDataSource.getRepository(UserQuestionResponse);
+
 
 export const createQuestionFromSeed = async (data: DeepPartial<Question>[]) => {
+    await userQuestionRepository.delete({});
     await optionRepository.delete({});
     await questionRepository.delete({});
     return await questionRepository.save(questionRepository.create(data))
